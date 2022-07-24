8ae251235f0105873c66681d9f9e72ec75165485
Sam Harvey <samuel.robert.harvey@gmail.com>
2022-07-24 21:39:34 +1200

    Update experiment params

--- analysis/calculate-gamma-stats.R
+++ analysis/calculate-gamma-stats.R
@@ -65,23 +65,22 @@ calculate_mle_summary_stats = function(
             file=file_name)
 }
 
-
 K = c(20, 100)
 m = c(50)
 m0 = c(10)
 gamma = c(1, 10)
 FH = c(0, 1, 5
-       # ,10, 20
+       ,10, 20
 )
 N_ref = c(1000, 500)
 OR = 1.5
 sim = 10
-# N_foc=c(200, 500)
+mu_delta = c(0,1)
 
-df_dist_variables = crossing(K, m, m0, gamma, FH, N_ref, OR, sim) %>% 
+df_dist_variables = crossing(K, m, m0, mu_delta, gamma, FH, N_ref, OR, sim) %>% 
   mutate(N_foc = case_when(N_ref == 1000 ~ 200,
                            N_ref == 500 ~ 500)) %>% 
-  mutate(simulation_file = glue("data/mle/mle_results_Sim_GenDif_m{m}_K{K}_Gamma{gamma}_OR{OR}_FH{FH}_Nref{N_ref}_Nfoc{N_foc}_sims{sim}.RData") %>% 
+  mutate(simulation_file = glue("data/mle/mle_results_Sim_GenDif_m{m}_K{K}_Gamma{gamma}_OR{OR}_FH{FH}_Nref{N_ref}_Nfoc{N_foc}_mudelta{mu_delta}_sims{sim}.RData") %>% 
            as.character())
 
 experiment_params = df_dist_variables[1,]
